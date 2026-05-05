6b423774a07ee66979776bc79b8ec685eaa0e726
dcoronado91 <derekcoronado9@gmail.com>
2026-05-05 09:30:33 -0600

    habilitar CORS para conexión con frontend

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -45,8 +45,23 @@ func main() {
 	log.Println("Servidor corriendo en http://localhost:" + port)
 
 	// Levantar servidor
-	err = http.ListenAndServe(":"+port, mux)
+	err = http.ListenAndServe(":"+port, enableCORS(mux))
 	if err != nil {
 		log.Fatal("Error al iniciar el servidor:", err)
 	}
+}
+
+func enableCORS(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+
+		if r.Method == http.MethodOptions {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
 }
